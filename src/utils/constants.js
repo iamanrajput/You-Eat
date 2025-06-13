cabbdf6310ad02d681d775c645825a71302c1ec0
Aman Raj <itzamanrajput@gmail.com>
2025-06-13 14:46:51 +0530

    You Eat

--- src/utils/constants.js
+++
@@ -1,6 +0,0 @@
-export const CDN_URL = 
-"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
-
-export const LOGO_URL = 
-"https://r2.erweima.ai/imgcompressed/compressed_edda597066c560d6cb8c2570b2876ab7.webp";
-
